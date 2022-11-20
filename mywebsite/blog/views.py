560a8dca79fb94b56a787f720500768eccadb83c
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-20 18:17:52 +0100

    blog feature

--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
+def index(request):
+  mytitle = Post.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mytitle': mytitle,
+  }
+  return HttpResponse(template.render(context, request))
 
-def blog(request):
-    return render(request, 'blog/mainblog.html')
+# def index(request):
+#     template = loader.get_template('myfirst.html')
+#     return HttpResponse(template.render())
+
