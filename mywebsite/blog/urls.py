f6681f07f0c3827baddc86eb97e6e4b495b3e20b
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-22 16:31:50 +0100

    blog almost done

--- mywebsite/blog/urls.py
+++ mywebsite/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', HomeView.as_view(), name='index'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail')
+    # path('', views.index, name='index')
 ]
--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -3,16 +3,25 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Post
 from django.views import generic
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+class HomeView(ListView):
+  model = Post
+  template_name = 'index.html'
+
+class ArticleDetailView(DetailView):
+  model = Post
+  template_name = 'article_details.html'
 
 
-
-def index(request):
-  mytitle = Post.objects.all().values()
-  template = loader.get_template('index.html')
-  context = {
-    'mytitle': mytitle,
-  }
-  return HttpResponse(template.render(context, request))
+# def index(request):
+#   mytitle = Post.objects.all().values()
+#   template = loader.get_template('index.html')
+#   context = {
+#     'mytitle': mytitle,
+#   }
+#   return HttpResponse(template.render(context, request))
 
 # def index(request):
 #     template = loader.get_template('myfirst.html')
