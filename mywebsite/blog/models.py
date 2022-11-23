8a90dbe29d3851b9ae18e7de1a5f80674ba48541
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-23 18:54:52 +0100

    Data time field

--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-
+    create_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
