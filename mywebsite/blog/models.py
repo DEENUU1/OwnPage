b26c6314f5568bddf98817eb50f9598c50d1862f
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-23 22:16:47 +0100

    blog main page

--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    small_body = models.TextField(max_length=100)
     create_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
     def __str__(self):
