1702e20ad71e21352fc786e0c519215419734e91
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-24 21:05:27 +0100

    ISSUE #3 DONE ISSUE #6 DONE ISSUE #5 DONE ISSUE #4 DONE

--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     body = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    time_reading = models.TextField()
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
