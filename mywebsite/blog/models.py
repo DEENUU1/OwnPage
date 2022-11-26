f275423cbbb715411d490efa209b7565559f776a
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-26 18:49:58 +0100

    #10

--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -6,7 +6,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-
+    create_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+    time_reading = models.TextField()
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
