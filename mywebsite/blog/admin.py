a3f6ec43b971c4d54adb7fd888ad28556985d41d
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-11-28 19:02:46 +0100

    #12

--- mywebsite/blog/admin.py
+++ mywebsite/blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Post
+from tinymce.widgets import TinyMCE
+from .models import *
+
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title, date, author, content')
+
+class PostAdmin(admin.ModelAdmin):
+
+    formfield_overrides = {
+        models.TextField: {'widget':TinyMCE()},
+    }
 
 admin.site.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title, date, author, content')
